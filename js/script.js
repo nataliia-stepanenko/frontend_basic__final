58c2deb77079e03893d1c149fd86dff5f0e609ec
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-02-02 10:59:42 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Carousel for team members
+
+$('.team__carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+//  Carousel for testimonials
+
+$('.one-time').slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+});
+
+//  Go to top
+
+$(document).ready(function() {
+      // Show or hide the sticky footer button
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+      });
+      
+      // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+      })
+    });
